00cebaddf3be63433cdc4dd77bb025180ccfa3d0
yuri7030 <yuriboykasgu@gmail.com>
2023-08-03 20:38:03 +0700

    restructure

--- main.go
+++ main.go
@@ -1,9 +1,21 @@
 package main
 
 import (
-	"github.com/diegovanne/go23/googleform/cmd"
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+	"github.com/yuri7030/final-project/Controllers"
 )
 
 func main() {
-	cmd.Execute()
+	r := gin.Default()
+
+	// Define the endpoints
+	r.POST("/surveys", Controllers.CreateSurvey)
+	r.GET("/surveys/:id/questions", Controllers.GetQuestions)
+	r.PUT("/surveys/:id/questions", Controllers.UpdateQuestions)
+
+	if err := r.Run(":8080"); err != nil {
+		fmt.Println("Error starting server:", err)
+	}
 }
