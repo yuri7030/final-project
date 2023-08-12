5dbfb9bc1365cdbfdc81b7c425a9292ead5945c8
yuri7030 <yuriboykasgu@gmail.com>
2023-08-12 11:53:34 +0700

    Update entity

--- internal/api/entities/answers.entity.go
+++ internal/api/entities/answers.entity.go
@@ -4,13 +4,13 @@ import "gorm.io/gorm"
 
 type Answer struct {
 	gorm.Model
-	QuestionID 		  uint
+	QuestionID        uint
 	GUID              string `gorm:"type:varchar(50)"`
 	AnswerText        string
 	SingleOptionID    uint
 	MultipleOptionIDs string `gorm:"type:varchar(50)"`
-	
-	Question Question `gorm:"foreignKey:QuestionID"`
+
+	Question *Question `gorm:"foreignKey:QuestionID"`
 }
 
 func (Answer) TableName() string {
